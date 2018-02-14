33befc4974b766ae8671d275186fe78e6b00cef6
BIT-USER <BIT-USER@DESKTOP-GO4E25Q>
2018-02-14 10:29:13 +0900

    gallery first

---
+++ src/main/java/com/javaex/dao/FileuploadDao.java
@@ -0,0 +1,21 @@
+package com.javaex.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.FileuploadVo;
+
+@Repository
+public class FileuploadDao {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public int insertUpload(FileuploadVo fileUploadVo) {
+		System.out.println("dao진입");
+		return sqlSession.insert("fileupload.insertUpload", fileUploadVo);
+	}
+}
